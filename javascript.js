862457abb859b5e2016bd43405fa6decb0f0cdd6
Valdemarcheck <valekzhaninvladimir58@gmail.com>
2022-09-21 17:39:30 +0500

    Add basic number operations to JS and a basic DOM structure

--- javascript.js
+++ javascript.js
@@ -1,1 +1,29 @@
-console.log('test');
+// math functions
+function sum(a,b) {
+    return a+b;
+}
+function subtract(a,b) {
+    return a-b;
+}
+function multiply(a,b) {
+    return a*b;
+}
+function divide(a,b) {
+    return a/b;
+}
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            sum(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+    }
+}
