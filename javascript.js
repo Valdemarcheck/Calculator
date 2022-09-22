5e910477d296a5b33f57d35562b114811a19036e
Valdemarcheck <valekzhaninvladimir58@gmail.com>
2022-09-22 19:20:35 +0500

    Add sqrt, percent, degree operations and 'result' button
    Before this commit, to do any operation we had to always put any operand
    after an expression (otherwise nothing would happen), which felt very
    annoying and mandatory. With 'result' button, this issue is resolved:
    when clicked, an expression automatically calculates a result, overriding
    the issue mentioned above

--- javascript.js
+++ javascript.js
@@ -1,35 +1,80 @@
 
-function operate(operator, num1, num2) {
+function operate(operator, num1, num2 = null) {
+    let result = 0;
     switch (operator) {
+        case '%':
+            result = num1 / 100;
+            break;
+        case '√':
+            result = Math.sqrt(num1);
+            break;
         case '+':
-            return num1 + num2;
+            result = num1 + num2;
+            console.log(result)
+            break;
         case '-':
-            return num1 - num2;
+            result = num1 - num2;
+            console.log(result)
+            break;
         case '*':
-            return num1 * num2;
+            result = num1 * num2;
+            break;
         case '/':
-            return num1 / num2;
+            result = num1 / num2;
+            break;
+        case '^':
+            result = num1 ** num2;
+            break;
     }
+    return parseFloat(result.toFixed(10));
+}
+
+const REGEX = new RegExp(`([+\-\/*^])`, 'g');
+const allowedOperands = ['+', '-', '/', '*', '^'];
+
+function doOperation(expr) {
+    if (typeof expr === 'object') expr = expr[0];
+
+    let exprNums = expr.split(/[+\-\/*^]/g);
+    if (exprNums.length > 2) {
+        exprNums.splice(0, 1);
+        exprNums[0] = exprNums[0]*-1+'';
+        expr = expr.slice(1, -1);
+    };
+
+    let exprSign = expr.match(/[+\-\/*^]/g)[0];
+
+    text = operate(exprSign, Number(exprNums[0]), Number(exprNums[1]))+'';
+    if (Number(text) >= Infinity || text === 'NaN')  text = '∞';
+    textObj.textContent = text;
 }
 
 // get screen text
 let textObj = document.querySelector('.screen-text');
 let text = '';
 
-// get all buttons except "Reset"
-const btns = [...document.querySelectorAll('.action-section button')];
+textObj.addEventListener('change', () => {
+    if (textObj.value.length < 1) textObj.value = 'what';
+});
 
-const numberBtns = btns.splice(0, 11);
-const clearBtn = btns.splice(0, 1);
-const operationBtns = btns.splice(0, 5);
-const deleteBtn = [operationBtns.pop()];
+// get all buttons number and operation buttons
+const numberBtns = Array.from(document.querySelectorAll('.number'));
+const operationBtns = Array.from(document.querySelectorAll('.operations > button'));
+const clearBtn = document.querySelector('.clear')
 
-deleteBtn[0].addEventListener('click', () => {
+const resultBtn = document.querySelector('.result');
+const deleteBtn = document.querySelector('.delete');
+const percentBtn = document.querySelector('.percent');
+const sqrtBtn = document.querySelector('.sqrt');
+
+// delete one character when DELETE button is pressed
+deleteBtn.addEventListener('click', () => {
     text = text.slice(0, -1);
     textObj.textContent = text;
 });
 
-clearBtn[0].addEventListener('click', () => {
+// clear screen text when CLEAR button is pressed
+clearBtn.addEventListener('click', () => {
     text = '';
     textObj.textContent = text;
 });
@@ -41,20 +86,50 @@ numberBtns.map(btn => btn.addEventListener('click', () => {
     textObj.textContent = text;
 }));
 
+// calculate results of an expression when the RESULT button was clicked
+resultBtn.addEventListener('click', () => {
+    if(text.length > 1) {
+        if (text.match(/(\d+[+\-\/*^]\d+)/)) doOperation(text);
+        else if (text.match(/(\d+[+\-\/*^])/)) text = text.slice(0, -1); 
+        textObj.textContent = text;
+    }
+}); 
+
+// perform a percent operation when a PERCENT button was clicked
+percentBtn.addEventListener('click', () => {
+    let textRaw = text + '%'; // how the text looks before application
+    if(textRaw.length > 1 && textRaw.match(/(\d+%)/)) {
+        let number = textRaw.split('%')[0];
+        text = operate('%', number);
+        textObj.textContent = text;
+    }
+});
+
+// perform a sqrt operation when a SQRT button was clicked
+sqrtBtn.addEventListener('click', () => {
+    if (text.match(/(\d+[+\-\/*^])/)) {
+        text = text.slice(0, -1);
+    }
+    if(text.match(/(\d+)/) || text.match(/(\d+\.\d+)/)) {
+        let number = Number(text);
+        text = operate('√', number)+'';
+        textObj.textContent = text;
+    }
+});
+
+// perform corresponding operation when a certain operation button was clicked (+, -, * or /)
 operationBtns.map(btn => btn.addEventListener('click', () => {
-    let textRaw = text + btn.textContent;
-    if(textRaw === '-' || textRaw.length !== 1) {
-        if(textRaw.match(/([+\-\/*]{2})/)) {
-            text = text.slice(0, -1);
+    let textRaw = text + btn.textContent; // how the text looks before application
+    if(textRaw === '-' || textRaw.length > 1) { // check whether the length of the screen text is less than 1
+        // check if textRaw ends with 2 operation signs in a row
+        if(textRaw.match(/([+\-\/*^]{2})/)) {
+            text = text.slice(0, -1); // delete previous sign and apply a new one
             text += btn.textContent;
             textObj.textContent = text;
-        } else {
-            let expr = text.match(/(\d+[+\-\/*]\d+)+/g);
-            if (expr) {
-                let exprNums = expr[0].split(/([+*\-\/])/);
-                let exprSign = expr[0].match(/([+*\-\/])/);
-                text = +parseFloat(operate(exprSign[0], Number(exprNums[0]), Number(exprNums[2])).toFixed(2));
-            }
+        } else { // else dissect screen text onto numbers and a sign, and perform a certain operation depending on that sign
+            let expr = text.match(/(\d+[+\-\/*^]\d+)/g);
+            // check if there are 2 numbers and a sign among them, and then perform calculations
+            if (expr) doOperation(expr);
             text += btn.textContent;
             textObj.textContent = text;
         }
